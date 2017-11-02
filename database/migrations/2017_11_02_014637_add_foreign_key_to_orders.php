e831e16b8036cb3d38fc87510c6ea8a4cf1cdcc8
Joseph Mancuso <idmann509@gmail.com>
2017-11-01 22:34:15 -0400

    added database migrations

---
+++ database/migrations/2017_11_02_014637_add_foreign_key_to_orders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyToOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('status')->after('user_id')->nullable()->unsigned();
+            $table->foreign('status')->references('id')->on('statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
