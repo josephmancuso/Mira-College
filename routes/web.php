2f5a70d57d036f272d7d37cc3a3d6269568b5fac
Joseph Mancuso <idmann509@gmail.com>
2017-11-01 22:34:54 -0400

    added routes for index, search and book

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/search', function () {
+    return view('search');
+});
+
+Route::get('/book', function () {
+    return view('book');
+});
+
+
